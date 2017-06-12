386ada132962add85551d988428ea8e0129cbdd1
meowoodie <woodielove@163.com>
2017-06-13 01:44:12 +0800

    test rabbitmq library

--- engine/test/test_dao.py
+++ engine/test/test_dao.py
@@ -4,12 +4,17 @@ from engine.rabbitmq_hub import PubSubHub, Pub, Sub
 # Test DAO class
 print (interfaces.Dao.get_access_token("yzg963@gmail.com", "yzg134530"))
 dao = interfaces.Dao("http://www.mageia.me/api/1.0.0/event_rules/", "yzg963@gmail.com", "yzg134530")
-print (dao["2r6Jq5jPDLhmNW4BjVzBzN"])
+print (dao["BberpR48TJQestDxPQmNaQ"])
+dao["BberpR48TJQestDxPQmNaQ"] = {
+	"rule_type": 0,
+	"rule_op": 0,
+	"sensor": "Fidwy7VuVS7TBGmH84oM47"
+}
 
 # Test Subscriber class
-interfaces.Subscriber("pubsub://leaniot:leaniot@119.254.211.60:5672/", "leaniot.realtime.data")
-while True:
-	pass
+# interfaces.Subscriber("pubsub://leaniot:leaniot@119.254.211.60:5672/", "leaniot.realtime.data")
+# while True:
+# 	pass
 
 # dao[""] = {
 #     "rule_op": "lt",
