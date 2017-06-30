6c2963d7f1907a97609eb4b92f3ea7091634f528
meowoodie <woodielove@163.com>
2017-06-30 15:24:00 +0800

    add test case for checker

---
+++ engine/test/test_checker.py
@@ -0,0 +1,13 @@
+from engine import checker
+
+checker = checker.RuleChecker(
+	dao_url="http://www.mageia.me/api/1.0.0/event_rules/",
+	sub_url="pubsub://leaniot:leaniot@119.254.211.60:5672/",
+	pub_url="pubsub://leaniot:leaniot@119.254.211.60:5672/",
+	email="yzg963@gmail.com",
+	password="yzg134530",
+	data_chn="leaniot.realtime.data",
+	notif_chn="leaniot.notification")
+
+while True:
+	pass
